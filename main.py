5c916f3f36ed4f85d2c4455f5e164327d6867ec7
PythonEESD2021 <ctran@eesd.org>
2021-01-12 00:31:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# Variables: containers, hold a value
+# Data Types/ Variable Types:
+#  String (str) : combination of characters (letters, numbers,    or symbols, must use quotation marks
+#  Integer (int) : whole numbers
+#  Character (char) : single letter, number, or symbol
+#  Float : numbers with decimals
+#  Boolean (bool) : True/ False
+
+# formula:
+# variableName = value
+# Rules for variable names
+#  1. always start with a letter
+#  2. can only contain numbers and letters
+#  3. if more than one word is used, use camelCase (first word       is lowercase and every word after that is uppercase)
+
+firstName = "Lalyn" #String type
+
+favBook = "Fundamentals of Material Balances" #string
+favNumber = 3 #
+isHungry = True #boolean
+favChar = 'l' #char
+pi = 3.14159
